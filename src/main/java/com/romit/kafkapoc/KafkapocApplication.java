fb19c70cdd398dfa4e77914c396487e5746de5fd
romit0226 <romitburi5@gmail.com>
2024-07-12 20:11:27 +0530

    First Commit

---
+++ src/main/java/com/romit/kafkapoc/KafkapocApplication.java
@@ -0,0 +1,13 @@
+package com.romit.kafkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkapocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkapocApplication.class, args);
+	}
+
+}
